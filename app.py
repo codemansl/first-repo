8046ce9f7c3e213b75656e29ce813ae93fb03383
codemansl <shirley.p.li@gmail.com>
2020-11-17 17:37:54 -0500

    modify app.py so that it would work with Openshift demo

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import os
 from flask import Flask
-
-app: Flask = Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+def main():
+    color = os.environ.get('COLOR', "green")
+    message = "Welcome !!! This is "+color+" environment"
+    return message
 
-if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
